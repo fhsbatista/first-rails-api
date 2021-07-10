b5acc34ba15960864b5961a7153862fbdf394ebf
Fernando Batista <fernandohsbatista@gmail.com>
2021-07-10 11:54:50 -0300

    some improvements on contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -39,13 +39,14 @@ class ContactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def contact_params
+    params.require(:contact).permit(:name, :email, :birthdate)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :contacts
+  # If we don't want rails to create all the urls of a resource
+  # We can specify the urls we want as below
+  # get "/contacts", to: "contacts#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
