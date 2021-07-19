6078f2a09b66228dd166ec36b9cdde56c4a67b11
Fernando Batista <fernandohsbatista@gmail.com>
2021-07-19 20:28:58 -0300

    Removed unnecessary fields from contact json rendering

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,3 @@
 class Contact < ApplicationRecord
   belongs_to :kind
-
-  def author
-    "Fernando Batista"
-  end
-
-  def kind_description
-    self.kind.description
-  end
-
-  def as_json(options = {})
-    super(
-      root: true,
-      methods: [:kind_description, :author],
-    )
-  end
 end
