9724b27f65d643e7c530d3aa4cdb42837abe27ed
Fernando Batista <fernandohsbatista@gmail.com>
2021-07-20 20:25:23 -0300

    Override birthdate field on contact model so that its format can adapt to the running language

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+
+  def birthdate
+    I18n.l(self[:birthdate])
+  end
+
+  def as_json(options = {})
+    super(options).merge ({ greeting: I18n.t(:hello) })
+  end
 end
